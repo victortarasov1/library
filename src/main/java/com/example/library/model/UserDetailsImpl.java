f65ecbb2b48e238f0d40d2d4517ac40d0aef32db
MorgothGorthaur <victortarasov13032001@gmail.com>
2022-12-17 13:24:30 +0200

    add implementation of userdetails

---
+++ src/main/java/com/example/library/model/UserDetailsImpl.java
@@ -0,0 +1,47 @@
+package com.example.library.model;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private Author author;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of(author.getRole());
+    }
+
+    @Override
+    public String getPassword() {
+        return author.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return author.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return author.getActuality().equals(Actuality.ACTIVE);
+    }
+}
